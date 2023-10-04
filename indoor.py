7536bef19db1fab679d503e27d1afa439e41a2cd
Linddaaa <lindakurtisa@gmail.com>
2023-10-04 09:29:49 +0300

    otrais

--- indoor.py
+++ indoor.py
@@ -13,10 +13,14 @@
 
 #03_uzdevums 
 
-
+# teksts = input("ievadi tekstu = ")
+# if ":)":
+#     print(teksts.replace(":)", "🙂"))
+# elif ":(":
+#     print(teksts.replace(":(", "🙁"))
 
 #04_uzdevums
-
+    
 # masa = int(input("ievadi masu kg ="))
 # c = 300000000
 # E = (masa * pow(c,2))
@@ -24,3 +28,14 @@
 
 
 #05_uzdevums 
+
+# def main():
+# dollars = dollars_to_float(input("How much was the meal? "))
+# percent = percent_to_float(input("What percentage would you like to tip? "))
+# tip = dollars * percent
+# print(f"Leave ${tip:.2f}")
+# def dollars_to_float(d):
+# # TODO
+# def percent_to_float(p):
+# # TODO
+# main()
