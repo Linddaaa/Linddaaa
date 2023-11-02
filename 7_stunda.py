473c8cd4b6a02d5e3c94947594152d54e0067198
Linddaaa <lindakurtisa@gmail.com>
2023-11-02 14:18:09 +0200

    stundas darbs

--- 7_stunda.py
+++ 7_stunda.py
@@ -1,2 +1,34 @@
-name = input("whats your name?")
-print(f"hello, {name}")
+# name = input("whats your name?")
+# print(f"hello, {name}")
+
+# name = input("whats your name?")
+# file = open("names.txt","w")
+# file.write(name)
+# file.close()
+
+
+# name = input("whats your name?")
+# file = open("names.txt","a")
+# file.write(f"{name}\n")
+# file.close()
+
+#noteikt tekstā cik rindiņas sākās ar burtu T
+# def line_count():
+#     file = open("story.txt", "r")
+#     count = 0
+#     for line in file:
+#         if line[0] not in 'T':
+#             count+=1
+#     file.close()
+#     print("No of lines not starting with 'T'=", count)
+
+# line_count()
+
+def burti_a(fails):
+    with open(fails, 'r') as f:
+        saturs = f.read()
+        skaits = saturs.count('a')
+    return skaits 
+fails = "teksts.txt"
+skaits = burti_a(fails)
+print(f"Failā '{fails}' ir {skaits} burti 'a'.")
