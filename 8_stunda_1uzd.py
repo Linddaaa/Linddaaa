fa315d3fb850df3f50dc133561269a9bb76097a0
Linddaaa <lindakurtisa@gmail.com>
2023-11-08 09:25:53 +0200

    stundas darbs

--- 8_stunda_1uzd.py
+++ 8_stunda_1uzd.py
@@ -1,3 +1,11 @@
 import csv
 import random
 
+with open('random_numbers.csv', 'w', newline="") as f:
+          writer = csv.writer(f)
+          writer.writerow(['Random numbers'])
+          for i in range(50):
+                random_number = random.randint(0, 1000)
+                writer.writerow([random_number])
+
+f.close()
