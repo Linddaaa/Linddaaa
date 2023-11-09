58b1ec5b3395f603b948ede0dcd4040076f80980
Linddaaa <lindakurtisa@gmail.com>
2023-11-09 14:32:12 +0200

    stundas darbs

--- 9_stunda.py
+++ 9_stunda.py
@@ -1,10 +1,16 @@
-import csv
+# import csv
 
-with open('fails.csv', mode ='r')as file:     #atver csv failu
-    csv_reader = csv.reader(file, delimiter=',')    #izveido csv lasītāja objektu
-    for ieraksts in csv_reader:     #ciklā nolasa visus ierakstus
-        Vārds = ieraksts[0]         #atlasa tikai vārda un nodarbošanās laukus
-        Nodarbošanās = ieraksts[3]
-        print(f"{Vārds}-{Nodarbošanās}")    #izdrukā ierakstu
+# with open('fails.csv', mode ='r')as file:     #atver csv failu
+#     csv_reader = csv.reader(file, delimiter=',')    #izveido csv lasītāja objektu
+#     for ieraksts in csv_reader:     #ciklā nolasa visus ierakstus
+#         Vards = ieraksts[0]         #atlasa tikai vārda un nodarbošanās laukus
+#         Nodarbosanas = ieraksts[3]
+#         print(f"{Vards}-{Nodarbosanas}")    #izdrukā ierakstu
 
-file.close()    #aizver csv failu         
+# file.close()    #aizver csv failu         
+
+
+import pandas
+
+csvFiles = pandas.read_csv('fails.csv')
+print(csvFiles)
