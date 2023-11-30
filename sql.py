58515048dee47392b7b5abf6540b6b7878c82a46
Linddaaa <lindakurtisa@gmail.com>
2023-11-30 13:32:36 +0200

    stundas darbs

---
+++ sql.py
@@ -0,0 +1,31 @@
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="KurtisaLinda05",
+#   database = "world"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("SELECT * FROM city")
+
+# myresult = mycursor.fetchall()
+
+# for x in myresult:
+#   print(x)
+
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="KurtisaLinda05"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE iedzīvotāji")
+
+
