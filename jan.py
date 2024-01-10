e083794ffe041f703d0beb8ee35bfe7cca047c14
Linddaaa <lindakurtisa@gmail.com>
2024-01-10 09:21:59 +0200

    linda

---
+++ jan.py
@@ -0,0 +1,77 @@
+# import turtle
+# turtle.penup()
+# turtle.backward(300)
+# turtle.pendown()
+# for i in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.forward(150)
+# turtle.pendown()
+# for i in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+
+#SAISTĪTI FOR CIKLI
+# import turtle
+# turtle.penup()
+# turtle.backward(300)
+# turtle.pendown()
+# for n in range (2):
+#     turtle.penup()
+#     turtle.forward(150)
+#     turtle.pendown()
+#     for i in range(4):
+#         turtle.forward(100)
+#         turtle.right(90)
+
+#WHILE CIKLS 
+# import turtle
+# turtle.penup()
+# turtle.backward(300)
+# turtle.pendown()
+# n = 0
+# while n < 2:
+#     turtle.penup()
+#     turtle.forward(150)
+#     turtle.pendown()
+#     n += 1
+#     i = 0
+#     while i < 4:
+#         turtle.forward(100)
+#         turtle.right(90)
+#         i += 1
+
+
+# import turtle 
+# turtle.showturtle()
+# def my_function(x, y):
+#     turtle.shape(x)
+#     turtle.pencolor(y)
+#     for x in range(10):
+#         if x % 2==0:
+#             turtle.forward(25)
+#             turtle.right(45)
+#         else:
+#             turtle.forward(25)
+#             turtle.right(45)
+
+# my_function("triangle", "blue")
+
+
+# import turtle 
+# turtle.showturtle()
+# def funkcija(x, y):
+#     turtle.shape(x)
+#     turtle.pencolor(y)
+#     for x in range(100):
+#         turtle.forward(10*x)
+#         turtle.right(90)
+# funkcija("turtle", "blue")
+
+
+
+
+import turtle 
+import random
+turtle.setpos(x,y)
--- read_file.py
+++
@@ -1,5 +0,0 @@
-f = open("mansteksts.txt", "r")
-f.write("Now the file has more con")
-for x in f: 
-    print(x)
-print(f.readline())
